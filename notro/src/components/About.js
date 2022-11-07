95ff7d1ae73959cc03af9e641912cd85587b962b
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-11-07 15:41:17 +0600

    updated(notro) - updated home.js

--- notro/src/components/About.js
+++ notro/src/components/About.js
@@ -1,16 +1,11 @@
-import React, { useContext } from 'react'
-import { useEffect } from 'react'
-import noteContext from '../context/notes/NoteContext'
+import React from 'react'
+// import { useEffect, useContext } from 'react'
+// import noteContext from '../context/notes/NoteContext'
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(()=> {
-    a.update();
-    // eslint-disable-next-line
-  }, [])
   return (
     <div>
-      this is about {a.state.name} and he's in class {a.state.class}
+      this is about
     </div>
   )
 }
--- notro/src/context/notes/NoteState.js
+++ notro/src/context/notes/NoteState.js
@@ -1,27 +1,10 @@
 import React from "react";
-import { useState } from "react";
+// import { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const NoteState = (props) => {
-    const defaultState = {
-        "name": "Gaurav",
-        "class": "5b"
-    }
-
-    const [state, setState] = useState(defaultState)
-
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                "name": "Yash",
-                "class": "12f"
-            })
-        }, 3000);
-    }
-
-    
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{ state:"state", update:"update" }}>
             {props.children}
         </NoteContext.Provider>
     )
