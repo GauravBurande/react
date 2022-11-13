299ac42f3a1f0e574a3770e5b788feb4c8406490
Gaurav Burande <100035026+GauravBurande@users.noreply.github.com>
2022-11-13 14:10:36 +0600

    REDUXAPP completed

--- REDUXAPP/src/App.js
+++ REDUXAPP/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Navbar';
+import Shop from './Shop';
 
 function App() {
   return (
     <>
     <Navbar/>
+    <Shop/>
     </>
     );
 }
--- REDUXAPP/src/Navbar.js
+++ REDUXAPP/src/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+    const amount = useSelector(state => state.amount)
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">Piggy Bank</a>
+                    <a className="navbar-brand" href="/">JOggER</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -15,6 +17,9 @@ const Navbar = () => {
                                 <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                         </ul>
+                        <div>
+                            <button disabled={true} className="btn btn-primary mx-3">your Balance : {amount}</button>
+                        </div>
                     </div>
                 </div>
             </nav>
